f37a59d06e5f2440e43484e186c689ed02cc354d
Solomon Ochepa <solomonochepa@gmail.com>
2023-03-23 04:46:13 +0100

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,77 +2,66 @@
 
 namespace Modules\User\app\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
+use App\Http\Controllers\Controller;
+use Modules\User\app\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * @return Renderable
      */
-    public function index()
-    {
-        return view('user::index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('user::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
+    public function index(): Response
     {
         //
     }
 
     /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
+     * Show the form for creating a new resource.
      */
-    public function show($id)
+    public function create(): Response
     {
-        return view('user::show');
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): Response
+    {
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
      */
-    public function edit($id)
+    public function edit(User $user): Response
     {
-        return view('user::edit');
+        //
     }
 
     /**
      * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user): RedirectResponse
     {
         //
     }
 
     /**
      * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
      */
-    public function destroy($id)
+    public function destroy(User $user): RedirectResponse
     {
         //
     }
---
+++ app/Models/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Modules\User\app\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\Profile;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Modules\Setting\app\Models\Setting;
+use Plank\Mediable\Mediable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, Mediable, HasRoles, SoftDeletes, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'username',
+        'phone',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Get user full name
+     */
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['first_name'] . ' ' . $attributes['last_name'],
+            // set: fn ($value, $attributes) => ''
+        );
+    }
+
+    public function settings()
+    {
+        return $this->morphMany(Setting::class, 'model');
+    }
+
+    public function profiles()
+    {
+        return $this->hasMany(Profile::class);
+    }
+}
