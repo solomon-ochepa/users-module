25c352f00e43eae2916d2038eb2c6ae74c1bba1d
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-02 16:31:07 +0100

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Modules\Setting\app\Models\Setting;
 use Plank\Mediable\Mediable;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -61,10 +60,5 @@ class User extends Authenticatable
             get: fn ($value, $attributes) => $attributes['first_name'] . ' ' . $attributes['last_name'],
             // set: fn ($value, $attributes) => ''
         );
-    }
-
-    public function settings()
-    {
-        return $this->morphMany(Setting::class, 'model');
     }
 }
