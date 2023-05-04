7cafec1328d48375c954a1e476d9de127c9f6a83
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-05-04 11:04:40 +0100

    Create UpdateUserRequest.php

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\User\app\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+        // return $this->user()->can('example.create');
+    }
+}
