a550e8620ea8d32a523b306ac53ee06cecfb87d1
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-05-04 11:06:10 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Modules\User\app\Http\Controllers\Admin\UserController;
+
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
 */
 
-Route::prefix('user')->group(function() {
-    Route::get('/', 'UserController@index');
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('user', UserController::class)->except(['index'])->names('user');
+    Route::get('users', [UserController::class, 'index'])->name('user.index');
+    Route::get('user/{user}/restore', [UserController::class, 'restore'])->name('user.restore');
+    Route::delete('user/{user}/permanent', [UserController::class, 'permanent'])->name('user.destroy.permanent');
+    Route::get('user', fn () => redirect(route('admin.user.index')));
 });
