98eab2f5f5433b9e4dcb2405bdfd99f49b6f5c65
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-05-04 11:06:51 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/Migrations/2014_10_12_000000_create_users_table.php
+++ database/Migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('nin')->nullable();
             $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('last_name');
